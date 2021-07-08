d80a9a5337aaed363749aa66aba3f4e5359b48a9
Aimee <aimeeharkin@gmail.com>
2021-07-08 13:21:08 +0200

    working but needs to look better

--- js/app.js
+++ js/app.js
@@ -21,3 +21,55 @@ const imageUrls = [
     'https://media.giphy.com/media/cO39srN2EUIRaVqaVq/source.gif'
 ];
 
+const students = ['Aimee', 'Priya', 'Mo', 'Carlos', 'Dalibor', 'Johnathan', 'Lui', 'Lucas', 'Shreekesh', 'Norman', 'Habid', 'Daniel', 'Shinhee', 'Francesco', 'Christoph', 'Tim'];
+
+const h1 = document.querySelector('h1');
+const btn = document.querySelector('#btn');
+const img = document.querySelector('#img');
+const containter = document.querySelector('.container');
+const textContainer = document.querySelector('.textContainer');
+
+const getRandomName = (students) => {
+    return students[Math.floor(Math.random() * students.length)]
+}
+
+const getRandomImage = (image) => {
+    return image[Math.floor(Math.random() * image.length)]
+}
+
+const updateHTML = () => {
+    let interval = setInterval(function(){
+        h1.innerText = getRandomName(students)
+    }, 500)
+
+    setTimeout(function(){
+        clearInterval(interval)
+    }, 4000)
+
+    setTimeout(function(){
+        let intervalImages = setInterval(function(){
+            let item = document.createElement('img');
+            item.src = getRandomImage(imageUrls);
+
+            item.classList.add('images');
+            document.body.appendChild(item);
+
+            console.log(item);
+
+            let x = Math.random()*100;
+            let y = Math.random()*100;
+            item.style.left = `${x-10}%`;
+            item.style.bottom= `${y-10}%`;
+
+        }, 1000)
+
+        setTimeout(function(){
+            clearInterval(intervalImages)
+        }, 7000)
+    })
+
+}
+
+btn.addEventListener('click', (e) => {
+    updateHTML()
+})
